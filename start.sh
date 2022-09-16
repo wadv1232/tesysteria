84ff51ba12cf61d2b4c26237d021209c69c37bc6
wzz <wangzhizhan@gmail.com>
2022-09-16 10:18:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -185,7 +185,8 @@ chmod 644 /etc/systemd/system/hysteria.service
 #systemctl enable hysteria
 install -m 755 /etc/systemd/system/hysteria.service /etc/init.d/hysteria
 rc-update add hysteria
-rc-service hysteria start
+#rc-service hysteria start
+/etc/init.d/hysteria start
 #systemctl start hysteria
 echo -e "\033[1;;35m\nwait...\n\033[0m"
 sleep 3
